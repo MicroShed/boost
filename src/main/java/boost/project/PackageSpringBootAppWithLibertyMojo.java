e91ac19d35350d256dc1407b7f028e0b24c8b1c2
Cheryl King <cheryllk@us.ibm.com>
2018-07-31 15:32:13 -0500

    Detect spring boot dependency version

--- src/main/java/boost/project/PackageSpringBootAppWithLibertyMojo.java
+++ src/main/java/boost/project/PackageSpringBootAppWithLibertyMojo.java
@@ -1,8 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
 package boost.project;
  
 import java.io.File;
 import java.net.URL;
+import java.util.List;
 
+import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugins.annotations.Mojo;
@@ -22,6 +34,8 @@ public class PackageSpringBootAppWithLibertyMojo extends AbstractMojo
 {
 
     String libertyServerName = "BoostServer";
+	String springBoot15 = "springBoot-1.5";
+	String springBoot20 = "springBoot-2.0";
 
 	@Parameter(defaultValue = "${project.build.directory}")
     private String projectBuildDir;
@@ -46,14 +60,30 @@ public class PackageSpringBootAppWithLibertyMojo extends AbstractMojo
                
         installApp();
 		
-		// Add springBoot-1.5 feature to server configDropins
+	// Add appropriate springBoot feature to server configDropins
+	String springBootVersion = findSpringBootVersion(mavenProject);
+
+	if (springBootVersion != null) {
+		String springBootFeature = null;
+		if (springBootVersion.startsWith("1.")) {
+			springBootFeature = springBoot15;
+		} else if (springBootVersion.startsWith("2.")) {
+			springBootFeature = springBoot20;
+		} else {
+			// log error for unsupported version
+			getLog().error("No supporting feature available in Open Liberty for org.springframework.boot dependency with version " + springBootVersion);
+		}
+
 		LibertyFeatureConfigGenerator featureConfig = new LibertyFeatureConfigGenerator();
-		featureConfig.addFeature("springBoot-1.5");
+		featureConfig.addFeature(springBootFeature);
 		featureConfig.writeToServer(projectBuildDir + "/liberty/wlp/usr/servers/" + libertyServerName);
+
+	}
+
 		
-		installMissingFeatures();
-		
-		createUberJar();
+	installMissingFeatures();
+	
+	createUberJar();
 		
     }
     
@@ -223,4 +253,19 @@ public class PackageSpringBootAppWithLibertyMojo extends AbstractMojo
     		)
 		);
 	}
+
+	/**
+	* Detect spring boot version dependency
+	*/
+	private String findSpringBootVersion(MavenProject project) {
+        	String version = null;
+        	List<Dependency> dependencies = project.getDependencies();
+        	for(Dependency dep: dependencies) {
+            		if("org.springframework.boot".equals(dep.getGroupId())){
+                		version = dep.getVersion();
+                		break;
+            		}
+        	}
+        	return version;
+    	}
 }
