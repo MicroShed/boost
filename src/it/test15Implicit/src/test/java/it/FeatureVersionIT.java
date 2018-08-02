b74dba29b6d4374709966e462deae110438d5d03
Scott Kurz <skurz@us.ibm.com>
2018-08-02 13:12:18 -0400

    Add to PR #23; rename "first" IT proj per convention

--- src/it/test15Implicit/src/test/java/it/FeatureVersionIT.java
+++ src/it/test15Implicit/src/test/java/it/FeatureVersionIT.java
@@ -20,7 +20,7 @@ import org.junit.Test;
 
 public class FeatureVersionIT {
 	
-	private static final String SPRING_BOOT_20_FEATURE = "<feature>springBoot-2.0</feature>";
+	private static final String SPRING_BOOT_15_FEATURE = "<feature>springBoot-1.5</feature>";
 	private static String TARGET_FEATURE_LIST_XML = "target/liberty/wlp/usr/servers/BoostServer/configDropins/overrides/featureList.xml";
     
     @Test
@@ -28,17 +28,17 @@ public class FeatureVersionIT {
     	File targetFile = new File(TARGET_FEATURE_LIST_XML);
     	assertTrue(targetFile.getCanonicalFile() + "does not exist.", targetFile.exists());
     	
-    	// Check contents of file for springBoot-20 feature
+    	// Check contents of file for springBoot-15 feature
     	boolean found = false;
     	BufferedReader br = new BufferedReader(new FileReader(TARGET_FEATURE_LIST_XML));
     	String line;
     	while ((line = br.readLine()) != null) {
-    	    if (line.contains(SPRING_BOOT_20_FEATURE)) {
+    	    if (line.contains(SPRING_BOOT_15_FEATURE)) {
     	    	found = true;
     	    	break;
     	    }
     	}
     	
-    	assertTrue("The "+SPRING_BOOT_20_FEATURE+" feature was not found in the server configuration", found);    
+    	assertTrue("The "+SPRING_BOOT_15_FEATURE+" feature was not found in the server configuration", found);    
     }
 }
