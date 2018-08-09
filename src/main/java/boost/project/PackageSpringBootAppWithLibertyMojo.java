d77fbe4526a9c4273491cf2c007306782156df61
Mike Andrasak <andrasak@us.ibm.com>
2018-08-09 15:22:14 -0400

    Issue 40 allow libertyMavenPlugin version to be configurable

--- src/main/java/boost/project/PackageSpringBootAppWithLibertyMojo.java
+++ src/main/java/boost/project/PackageSpringBootAppWithLibertyMojo.java
@@ -39,6 +39,9 @@ public class PackageSpringBootAppWithLibertyMojo extends AbstractMojo implements
 
     String libertyMavenPluginGroupId = "net.wasdev.wlp.maven.plugins";
     String libertyMavenPluginArtifactId = "liberty-maven-plugin";
+    
+    //@Parameter(property = "package-app.libertyMavenPluginVersion", defaultValue = "2.5.1-SNAPSHOT")
+    //String libertyMavenPluginVersion;
     String libertyMavenPluginVersion = "2.5.1-SNAPSHOT";
     
     @Parameter(defaultValue = "${project.build.directory}")
