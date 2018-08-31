954d2b8fb6cbdf75e82fd03a8b1d6d314ace5b41
Anjum Fatima <anjum.fatima@ibm.com>
2018-08-31 09:56:33 -0500

    Integration tests for docker-build and docker-push

---
+++ boost-maven-plugin/src/it/test-docker/src/test/java/application/DockerBuildIT.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+
+package application;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import org.junit.Test;
+import org.junit.BeforeClass;
+import java.util.List;
+
+import com.github.dockerjava.api.DockerClient;
+import com.github.dockerjava.api.command.CreateContainerResponse;
+import com.github.dockerjava.api.model.Container;
+import com.github.dockerjava.api.model.PortBinding;
+import com.github.dockerjava.core.DockerClientBuilder;
+
+import org.springframework.boot.SpringBootVersion;
+
+
+public class DockerBuildIT {
+    private static File dockerFile;
+    private static DockerClient dockerClient;
+        
+    @BeforeClass
+    public static void setup() throws Exception {
+        dockerFile = new File("Dockerfile");
+        dockerClient = DockerClientBuilder.getInstance().build();
+    }    
+    
+    @Test
+    public void testDockerizeCreatesDockerfile() throws Exception {
+        assertTrue(dockerFile.getCanonicalPath() + " was not created", dockerFile.exists());
+    }
+    
+    @Test
+    public void testDockerfileContainsCorrectLibertyImage() throws Exception {
+        BufferedReader reader = new BufferedReader(new FileReader(dockerFile));
+        String line = reader.readLine();
+        String version = SpringBootVersion.getVersion();
+        if(version !=null) {
+            if(version.startsWith("1.")) {
+                assertTrue("Expected Open liberty base image open-liberty:springBoot1 was not found in " + dockerFile.getCanonicalPath(), line.contains("open-liberty:springBoot1"));
+            } else if (version.startsWith("2.")) {
+                assertTrue("Expected Open liberty base image open-liberty:springBoot2 was not found in " + dockerFile.getCanonicalPath(), line.contains("open-liberty:springBoot2"));
+            }
+        }
+    }
+    
+    @Test
+    public void runDockerContainerAndVerifyAppOnEndpoint() throws Exception {
+        CreateContainerResponse container = dockerClient.createContainerCmd("localhost:5000/test-docker:latest").withPortBindings(PortBinding.parse("9080:9080")).exec();
+        Thread.sleep(3000);
+        
+        dockerClient.startContainerCmd(container.getId()).exec();
+        
+        Thread.sleep(3000);
+        testDockerContainerRunning();
+        
+        Thread.sleep(10000);
+        testAppRunningOnEndpoint();
+
+        dockerClient.stopContainerCmd(container.getId()).exec();
+
+        dockerClient.removeContainerCmd(container.getId()).exec();
+    }
+    
+    public void testDockerContainerRunning() throws Exception{
+        List<Container> containers = dockerClient.listContainersCmd().exec();
+        //docker local registry conatiner and image container
+        assertEquals("Expected number of running containers not found", 2, containers.size());
+    }
+    
+    public void testAppRunningOnEndpoint() throws Exception{
+        URL requestUrl = new URL("http://localhost:9080/spring/");
+        HttpURLConnection conn = (HttpURLConnection) requestUrl.openConnection();
+
+        if (conn != null) {
+            assertEquals("Expected response code not found.", 200, conn.getResponseCode());
+        }
+
+        StringBuffer response = new StringBuffer();
+
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                response.append(line);
+            }
+        }
+        assertEquals("Expected body not found.", "Greetings from Spring Boot!", response.toString());
+    }
+}
