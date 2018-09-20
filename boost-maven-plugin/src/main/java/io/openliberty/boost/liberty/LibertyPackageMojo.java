f68d4746d2562859c44714c132fc8618bf240a39
Charles Tian <ctian@us.ibm.com>
2018-09-20 14:10:45 -0500

    Add back acceptLicense for 18.0.0.2

--- boost-maven-plugin/src/main/java/io/openliberty/boost/liberty/LibertyPackageMojo.java
+++ boost-maven-plugin/src/main/java/io/openliberty/boost/liberty/LibertyPackageMojo.java
@@ -326,8 +326,9 @@ public class LibertyPackageMojo extends AbstractLibertyMojo {
      * configDropins.
      *
      */
-    private void installMissingFeatures() throws MojoExecutionException {
-        executeMojo(getPlugin(), goal("install-feature"), configuration(element(name("serverName"), libertyServerName)), getExecutionEnvironment());
+    private void installMissingFeatures() throws MojoExecutionException {        
+        executeMojo(getPlugin(), goal("install-feature"), configuration(element(name("serverName"), libertyServerName),
+                element(name("features"), element(name("acceptLicense"), "true"))), getExecutionEnvironment());
     }
 
     /**
