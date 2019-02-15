8815917e1fcae09eb74638cce4d4a6c07f07f1a5
Charles Tian <ctian@us.ibm.com>
2019-02-15 15:48:28 -0600

    Test jpa 2.2 and same installed features regardless of definition of transitive jdbc booster

--- boost-maven/boost-maven-plugin/src/it/test-jpa-2.1/src/main/java/application/Thing.java
+++ boost-maven/boost-maven-plugin/src/it/test-jpa-2.1/src/main/java/application/Thing.java
@@ -1,4 +1,14 @@
-package application.servlet;
+/*******************************************************************************
+ * Copyright (c) 2018 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *******************************************************************************/
+package application;
 
 import java.io.Serializable;
 
