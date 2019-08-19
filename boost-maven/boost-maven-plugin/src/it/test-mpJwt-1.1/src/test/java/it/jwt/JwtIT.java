be1fb2de42e701f7b1c5e6dfc701841fc24c73a2
Mike Andrasak <andrasak@us.ibm.com>
2019-08-19 09:03:27 -0400

    Issue 334 mpjwt updates from code review

--- boost-maven/boost-maven-plugin/src/it/test-mpJwt-1.1/src/test/java/it/jwt/JwtIT.java
+++ boost-maven/boost-maven-plugin/src/it/test-mpJwt-1.1/src/test/java/it/jwt/JwtIT.java
@@ -29,79 +29,71 @@ import java.net.URLClassLoader;
 
 public class JwtIT {
 
-  private final String TESTNAME = "TESTUSER";
-  private final String INV_JWT = "/inventory/jwt";
+    private final String TESTNAME = "TESTUSER";
+    private final String INV_JWT = "/inventory/jwt";
 
-  String baseUrl;
+    String baseUrl;
 
-  String authHeader;
-  
-  String unauthHeader;
-  
-  
+    String authHeader;
 
-  @Before
-  public void setup() throws Exception {
-          // These tests is being temporarily skipped when running on TomEE
-          // until the failures are addressed.
-          String runtime = System.getProperty("boostRuntime");
-          org.junit.Assume.assumeTrue("ol".equals(runtime) || "wlp".equals(runtime));
-          String port = System.getProperty("boost.http.port");
-          baseUrl = "http://localhost:" + port;
+    String unauthHeader;
 
+    @Before
+    public void setup() throws Exception {
+        // These tests is being temporarily skipped when running on TomEE
+        // until the failures are addressed.
+        String runtime = System.getProperty("boostRuntime");
+        org.junit.Assume.assumeTrue("ol".equals(runtime) || "wlp".equals(runtime));
+        String port = System.getProperty("boost.http.port");
+        baseUrl = "http://localhost:" + port;
 
-	      JwtVerifier jwtvf = new JwtVerifier();
-	      authHeader = "Bearer " + jwtvf.createJwt(TESTNAME,"groups=admin");
-	      unauthHeader = "Bearer " + jwtvf.createJwt(TESTNAME,"groups=user");
-	      String publicKeyLocation = System.getProperty("mp.jwt.verify.publickey.location");
-	      if(publicKeyLocation != null )
-	         JwtBuilder.storePublicKey(publicKeyLocation);
-	      else 
-	    	     throw new Exception("public key location is null");
-  }
-
-  @Test
-  public void testSuite() {
-	//this.testPublicKeyLocation();
-    this.testJwtGetName(true);
-    this.testJwtGetCustomClaim(true);
-    this.testJwtGetCustomClaim(false);
-  }
-
-  
-  public void testJwtGetName(boolean userAuthorized) {
-    String jwtUrl = baseUrl + INV_JWT + "/username";
-    Response jwtResponse = TestUtils.processRequest(jwtUrl, "GET", null, authHeader);
-
-    assertEquals(
-        "HTTP response code should have been " + Status.OK.getStatusCode() + ".",
-        Status.OK.getStatusCode(), jwtResponse.getStatus());
-
-    String responseName = jwtResponse.readEntity(String.class);
-
-    assertEquals("The test name and jwt token name should match", TESTNAME,
-        responseName);
-    System.out.println("the testname = " + responseName);
-  }
-
-  public void testJwtGetCustomClaim(boolean userAuthorized) {
-    String jwtUrl = baseUrl + INV_JWT + "/customClaim";
-    if(userAuthorized) {
-      Response jwtResponse = TestUtils.processRequest(jwtUrl, "GET", null, authHeader);
-      assertEquals("HTTP response code should have been "
-            + Status.OK.getStatusCode() + ".", Status.OK.getStatusCode(),
-            jwtResponse.getStatus());
-      System.out.println("Authorized header custom claim = " + jwtResponse.readEntity(String.class));
-    } else {
-        Response jwtResponse = TestUtils.processRequest(jwtUrl, "GET", null, unauthHeader);
-        assertEquals("HTTP response code should have been "
-              + Status.FORBIDDEN.getStatusCode() + ".", Status.FORBIDDEN.getStatusCode(),
-              jwtResponse.getStatus());
-        System.out.println("Unauthorized header response code  = " + jwtResponse.getStatus()); 
+        JwtVerifier jwtvf = new JwtVerifier();
+        // create header that should be authorized
+        authHeader = "Bearer " + jwtvf.createJwt(TESTNAME, "groups=admin");
+        // create header that should NOT be authorized
+        unauthHeader = "Bearer " + jwtvf.createJwt(TESTNAME, "groups=user");
+        String publicKeyLocation = System.getProperty("mp.jwt.verify.publickey.location");
+        if (publicKeyLocation != null)
+            JwtBuilder.storePublicKey(publicKeyLocation);
+        else
+            throw new Exception("public key location is null");
     }
-    	
 
-  }
+    @Test
+    public void testSuite() {
+        this.testJwtGetName(true);
+        this.testJwtGetCustomClaim(true);
+        this.testJwtGetCustomClaim(false);
+    }
+
+    public void testJwtGetName(boolean userAuthorized) {
+        String jwtUrl = baseUrl + INV_JWT + "/username";
+        Response jwtResponse = TestUtils.processRequest(jwtUrl, "GET", null, authHeader);
+
+        assertEquals("HTTP response code should have been " + Status.OK.getStatusCode() + ".",
+                Status.OK.getStatusCode(), jwtResponse.getStatus());
+
+        String responseName = jwtResponse.readEntity(String.class);
+
+        assertEquals("The test name and jwt token name should match", TESTNAME, responseName);
+        System.out.println("the testname = " + responseName);
+    }
+
+    public void testJwtGetCustomClaim(boolean userAuthorized) {
+        String jwtUrl = baseUrl + INV_JWT + "/customClaim";
+        if (userAuthorized) {
+            Response jwtResponse = TestUtils.processRequest(jwtUrl, "GET", null, authHeader);
+            assertEquals("HTTP response code should have been " + Status.OK.getStatusCode() + ".",
+                    Status.OK.getStatusCode(), jwtResponse.getStatus());
+            System.out.println("Authorized header custom claim = " + jwtResponse.readEntity(String.class));
+        } else {
+            Response jwtResponse = TestUtils.processRequest(jwtUrl, "GET", null, unauthHeader);
+            assertEquals("HTTP response code should have been " + Status.FORBIDDEN.getStatusCode() + ".",
+                    Status.FORBIDDEN.getStatusCode(), jwtResponse.getStatus());
+            System.out.println("Unauthorized header response code  = " + jwtResponse.getStatus());
+        }
+
+    }
 
 }
 // end::test[]
--- boost-maven/boost-runtimes/runtime-openliberty/src/main/java/boost/runtimes/openliberty/boosters/LibertyMPJWTBoosterConfig.java
+++ boost-maven/boost-runtimes/runtime-openliberty/src/main/java/boost/runtimes/openliberty/boosters/LibertyMPJWTBoosterConfig.java
@@ -5,6 +5,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
+ *
  * Contributors:
  *     IBM Corporation - initial API and implementation
  *******************************************************************************/
@@ -20,22 +21,22 @@ import boost.runtimes.openliberty.LibertyServerConfigGenerator;
 import boost.runtimes.openliberty.boosters.LibertyBoosterI;
 
 public class LibertyMPJWTBoosterConfig extends MPJWTBoosterConfig implements LibertyBoosterI {
-	
-  public LibertyMPJWTBoosterConfig(Map<String, String> dependencies, BoostLoggerI logger) throws BoostException {
-	       super(dependencies, logger);
-  }
-		
-  @Override
-  public String getFeature() {        
-	  if (getVersion().equals(MP_JWT_VERSION_20)) {
-		  return MPJWT_11;
-		  }
-	      return null;
-      }
 
-  @Override
-  public void addServerConfig(LibertyServerConfigGenerator libertyServerConfigGenerator) {
-	  
-  }
-  
+    public LibertyMPJWTBoosterConfig(Map<String, String> dependencies, BoostLoggerI logger) throws BoostException {
+        super(dependencies, logger);
+    }
+
+    @Override
+    public String getFeature() {
+        if (getVersion().equals(MP_JWT_VERSION_11)) {
+            return MPJWT_11;
+        }
+        return null;
+    }
+
+    @Override
+    public void addServerConfig(LibertyServerConfigGenerator libertyServerConfigGenerator) {
+
+    }
+
 }
