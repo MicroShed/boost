7e63c183fa5f133ac62efb73af0ddcc316021a14
Andrew J. Mauer <ajmauer@gmail.com>
2019-08-21 17:27:20 -0400

    add new beanValidation booster

--- boost-common/src/main/java/boost/common/boosters/AbstractBoosterConfig.java
+++ boost-common/src/main/java/boost/common/boosters/AbstractBoosterConfig.java
@@ -42,6 +42,7 @@ public abstract class AbstractBoosterConfig {
     public static final String JSONP_VERSION_11 = "1.1-" + boostVersion + "-" + milestone + "-SNAPSHOT";
     public static final String JPA_VERSION_21 = "2.1-" + boostVersion + "-" + milestone + "-SNAPSHOT";
     public static final String JPA_VERSION_22 = "2.2-" + boostVersion + "-" + milestone + "-SNAPSHOT";
+    public static final String BEANVALIDATION_VERSION_20 = "2.0-" + boostVersion + "-" + milestone + "-SNAPSHOT";
     public static final String MP_HEALTH_VERSION_10 = "1.0-" + boostVersion + "-" + milestone + "-SNAPSHOT";
     public static final String MP_CONFIG_VERSION_13 = "1.3-" + boostVersion + "-" + milestone + "-SNAPSHOT";
     public static final String MP_METRICS_VERSION_11 = "1.1-" + boostVersion + "-" + milestone + "-SNAPSHOT";
